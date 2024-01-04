1f49b0257f0154d8f1dcef5f0c7e0eb2e78bf57a
tmgasek <tomaszgasek28@gmail.com>
2024-01-04 16:11:45 +0000

    feat: flags

--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-const (
-	screenWidth  = 720
-	screenHeight = 480
+var (
+	screenWidth  int
+	screenHeight int
+	cellDensity  float64
 )
 
 func main() {
+	width := flag.Int("width", 720, "screen width")
+	height := flag.Int("height", 480, "screen height")
+	density := flag.Float64("density", 1.0, "cell density")
+	flag.Parse()
+
+	screenWidth = *width
+	screenHeight = *height
+	cellDensity = *density
+
 	g := &Game{
-		world: NewWorld(screenWidth, screenHeight, int((screenWidth*screenHeight)/5)),
+		world: NewWorld(screenWidth, screenHeight, int(float64(screenWidth*screenHeight)/10*cellDensity)),
 	}
 
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
